126728fdd303bebadf56217e5e8f02ba83934c48
Bolaji87 <sakabolaji77@gmail.com>
2025-03-02 01:35:35 +0100

    feat: call Header and AppLayout components in Homepage

--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import styles from "./Homepage.module.css";
+import AppLayout from "../AppLayout/AppLayout";
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.home}>
+      <Header />
+      <AppLayout />
+    </div>
+  );
 }
 
 export default Homepage;
