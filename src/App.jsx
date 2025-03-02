63067411f9fced4fd112ab97bb26154fea9516ce
Bolaji87 <sakabolaji77@gmail.com>
2025-03-02 01:45:49 +0100

    feat: call appRoutes and map it inside BrowserRouter,Routes and Route from react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { appRoutes } from "./Logic/routes";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {appRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
