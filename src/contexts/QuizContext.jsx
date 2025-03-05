8b7aded0189f7e13230b3ad244ebcf1f126bc6b6
Bolaji87 <sakabolaji77@gmail.com>
2025-03-05 15:15:00 +0100

    feat: add nextQuestion case to reducer to render the next question in UI

--- src/contexts/QuizContext.jsx
+++ src/contexts/QuizContext.jsx
@@ -6,6 +6,7 @@ const initialState = {
   questions: [],
   status: "loading",
   index: 0,
+  answer: null,
 };
 
 // status : can be 'loading', 'error', 'ready', 'active' and 'finished'
@@ -18,16 +19,30 @@ function reducer(state, action) {
       return { ...state, error: action.payload, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "choose":
+      return { ...state, answer: action.payload };
+    case "nextQuestion": {
+      return {
+        ...state,
+        index: state.index < 23 && state.index + 1,
+        answer: null,
+      };
+    }
+    case "prevQuestion":
+      return { ...state, index: state.index > 0 && state.index - 1 };
     default:
       throw new Error("unkwown action type");
   }
 }
 
 function QuizProvider({ children }) {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
+
+  const numQuestions = questions.length;
+
   useEffect(function () {
     async function getQuestions() {
       try {
@@ -43,7 +58,9 @@ function QuizProvider({ children }) {
     getQuestions();
   }, []);
   return (
-    <QuizContext.Provider value={{ dispatch, questions, status, index }}>
+    <QuizContext.Provider
+      value={{ dispatch, questions, status, index, answer, numQuestions }}
+    >
       {children}
     </QuizContext.Provider>
   );
