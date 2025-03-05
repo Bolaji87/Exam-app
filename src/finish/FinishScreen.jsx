fe74c3767a9cd27b84081792610d8ae685462b25
Bolaji87 <sakabolaji77@gmail.com>
2025-03-05 15:35:51 +0100

    feat: create FinishScreen component and it's css file

---
+++ src/finish/FinishScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./FinishScreen.module.css";
+
+function FinishScreen() {
+  return (
+    <div>
+      <p>You Exam has been submitted</p> <span> you Score X/X</span>
+    </div>
+  );
+}
+
+export default FinishScreen;
