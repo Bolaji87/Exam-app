ba10c6c870f7a4b53859b360d59d74f3af88a04c
Bolaji87 <sakabolaji77@gmail.com>
2025-03-05 15:16:53 +0100

    feat: changes

--- src/components/start/StartScreen.jsx
+++ src/components/start/StartScreen.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useQuiz } from "../../hooks/useQuiz";
+import styles from "./StartScreen.module.css";
 
 function StartScreen() {
-  const { dispatch } = useQuiz();
+  const { dispatch, numQuestions } = useQuiz();
   return (
-    <div>
+    <div className={styles.startCont}>
       <h2>Welcome to React Exam App 2025</h2>
-      <button onClick={() => dispatch({ type: "start" })}>
-        let's get started
-      </button>
+      <p>
+        This exam contains {numQuestions} questions , where you can test your
+        knowledge based on what was covered so far in the course
+      </p>
+      <button onClick={() => dispatch({ type: "start" })}>Start</button>
     </div>
   );
 }
