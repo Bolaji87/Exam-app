7ffe4774121256a9a39bbeb6120cab5fb03798c2
Bolaji87 <sakabolaji77@gmail.com>
2025-03-05 15:16:25 +0100

    feat: add changes to Questions component and styles

--- src/components/question/Questions.jsx
+++ src/components/question/Questions.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import Options from "../option/Options";
 import { useQuiz } from "../../hooks/useQuiz";
+import styles from "./Question.module.css";
 
 function Questions() {
-  const { questions, index } = useQuiz();
+  const { questions, index, dispatch, answer } = useQuiz();
   const question = questions.at(index);
+
   return (
-    <div>
-      {question.question}
-      <div>
-        {question.options.map((option) => (
-          <button key={option}>{option}</button>
+    <div className={styles.question}>
+      <h2>
+        {index + 1} : {question.question}
+      </h2>
+      <ul>
+        {question.options.map((option, optionIndex) => (
+          <Options key={option} option={option} optionIndex={optionIndex} />
         ))}
+      </ul>
+
+      <div className={styles.btns}>
+        {/* {answer !== null && (
+          <button onClick={() => dispatch({ type: "prevQuestion" })}>
+            Previous
+          </button>
+        )} */}
+        {answer !== null && (
+          <button onClick={() => dispatch({ type: "nextQuestion" })}>
+            Next
+          </button>
+        )}
       </div>
     </div>
   );
